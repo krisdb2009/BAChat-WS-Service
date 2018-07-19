fb4680aac81eae9c566b776047af50a9017028ec
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-07-18 22:24:37 -0400

    Add project files.

---
+++ WebSockets/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SuperSocket.WebSocket;
+
+namespace WebSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server...");
+            WebSocketServer ws = new WebSocketServer();
+            ws.Setup(1234);
+            ws.Start();
+            ws.NewSessionConnected += Ws_NewSessionConnected;
+            ws.NewMessageReceived += Ws_NewMessageReceived;
+            Console.WriteLine("Ready.");
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void Ws_NewMessageReceived(WebSocketSession session, string value)
+        {
+            Console.WriteLine(session.RemoteEndPoint.Address + ":" + session.RemoteEndPoint.Port + ": " + value);
+            session.Send(value);
+        }
+
+        private static void Ws_NewSessionConnected(WebSocketSession session)
+        {
+            Console.WriteLine(session.RemoteEndPoint.Address + ":" + session.RemoteEndPoint.Port + " connected.");
+        }
+    }
+}
